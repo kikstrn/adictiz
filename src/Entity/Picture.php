9d7094860d095c83bfb239c830129297cc0aa1d3
Tournon Kilian <tournonklian@air-de-tournon.home>
2024-08-22 20:00:05 +0200

    add front and back file upload

---
+++ src/Entity/Picture.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PictureRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PictureRepository::class)]
+class Picture
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::ARRAY)]
+    private array $files = [];
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFiles(): array
+    {
+        return $this->files;
+    }
+
+    public function setFiles(array $files): static
+    {
+        $this->files = $files;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+}
---
+++ src/Form/PictureType.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Picture;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
+use Symfony\Component\Validator\Constraints\All;
+
+class PictureType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+        ->add('files', FileType::class, [
+            'label' => 'Insérez une ou plusieurs images',
+            'data_class'=> null,
+            'empty_data' => '',
+            'multiple' => true,
+            'mapped' => false,
+            'required' => true,
+            'constraints' => [
+                new All([
+                    'constraints' => [
+                        new File([
+                        'maxSize' => '5M',
+                        'mimeTypes' => [
+                                        'application/pdf',
+                                        'image/*',
+                                        'application/x-pdf',
+                                        'text/plain',
+                        ]
+                    ]),
+                ],
+            ]),
+        ]
+        ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Picture::class,
+        ]);
+    }
+}
